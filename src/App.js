309e56ea75e6b859a724f30b24a7425ee47cdb88
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-03-05 07:32:47 -0700

    removed nav import from app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import Header from './components/Header';
-import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -33,7 +32,7 @@ function App() {
  <div>
  <Header handlePageChange={handlePageChange}currentPage={currentPage}/>
  {/* We are passing the currentPage from state and the function to update it */}
- <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+ {/* <Nav currentPage={currentPage} handlePageChange={handlePageChange} /> */}
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
  </div>
