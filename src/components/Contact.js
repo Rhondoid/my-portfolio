8d45ea2a763c82614504c4b760fadf5e7a703859
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-03-05 15:01:48 -0700

    email validation functional

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,19 @@
-import React from 'react';
-// import { Form, Field, ErrorMessage } from 'react';
-import { useForm } from "react-hook-form";
+
+import React, { useState } from "react";
+import validator from 'validator'
 
 export default function Contact() {
+  const [emailError, setEmailError] = useState('')
+  const validateEmail = (e) => {
+    var email = e.target.value
+  
+    if (validator.isEmail(email)) {
+      setEmailError('Valid Email :)')
+    } else {
+      setEmailError('Enter valid Email!')
+    }
+  }
+  
   return (
     <form >
       <label>
@@ -12,9 +23,15 @@ export default function Contact() {
       <br></br>
       <label>
         email:
-        <input type="text"  />
+        <input type="text" onChange={(e) => validateEmail(e)}></input> 
+
       </label>
       <br></br>
+      <span style={{
+          fontWeight: 'bold',
+          color: 'red',
+        }}>{emailError}</span>
+        <br></br>
       <label>
         message:
         <input type="text"  />
