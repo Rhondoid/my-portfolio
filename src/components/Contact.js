8f0d85ae09fb6d85a491cd295e0f46b3865362c0
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-03-05 14:40:42 -0700

    contact form functional

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,63 +1,16 @@
 import React from 'react';
 // import { Form, Field, ErrorMessage } from 'react';
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
   return (
-    <div>
-      <h2>Contact Page</h2>
-      {/* <React
-      initialValues={{ name: '', email: '', message: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.name) {
-          errors.name = 'Required';
-        }
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-          errors.email = 'Invalid email address';
-        }
-        if (!values.message) {
-          errors.message = 'Required';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <div>
-            <label htmlFor="name">Name</label>
-            <Field type="text" name="name" />
-            <ErrorMessage name="name" component="div" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-          </div>
-          <div>
-            <label htmlFor="message">Message</label>
-            <Field as="textarea" name="message" />
-            <ErrorMessage name="message" component="div" />
-          </div>
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </React> */}
-    <div className='contact'>
-      <li>Phone# 555-555-5555</li>
-      <li><a href="https://github.com/Rhondoid">GitHub</a></li>
-      <li><a href="https://www.linkedin.com/feed/">LinkedIn</a></li>
-    </div>
-    </div>
-    
+    <form >
+      <label>
+        Name:
+        <input type="text"  />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
-}
+  
+  }
